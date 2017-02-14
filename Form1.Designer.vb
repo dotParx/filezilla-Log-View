ef3f7503dc0e0274519672abea097b90f698923f
Shawn Parker <Shawn@parxmedia.com>
2017-02-14 12:37:17 -0500

    MultiThreading
    Not optimized.
    Multi-threaded so it doesn't drive me nuts.

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -23,10 +23,15 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SourceFile = New System.Windows.Forms.TextBox()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.RETR = New System.Windows.Forms.CheckBox()
         Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.topMain = New System.Windows.Forms.SplitContainer()
+        Me.TopLeft = New System.Windows.Forms.SplitContainer()
+        Me.lblUser = New System.Windows.Forms.Label()
+        Me.userSearch = New System.Windows.Forms.TextBox()
+        Me.LinesCounted = New System.Windows.Forms.Label()
         Me.logdata = New System.Windows.Forms.SplitContainer()
         Me.logView = New System.Windows.Forms.DataGridView()
         Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
@@ -35,12 +40,20 @@ Partial Class Form1
         Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Column5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.exportData = New System.Windows.Forms.Button()
-        Me.lblUser = New System.Windows.Forms.Label()
-        Me.userSearch = New System.Windows.Forms.TextBox()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.bgWorker = New System.ComponentModel.BackgroundWorker()
         CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SplitContainer1.Panel1.SuspendLayout()
         Me.SplitContainer1.Panel2.SuspendLayout()
         Me.SplitContainer1.SuspendLayout()
+        CType(Me.topMain, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.topMain.Panel1.SuspendLayout()
+        Me.topMain.Panel2.SuspendLayout()
+        Me.topMain.SuspendLayout()
+        CType(Me.TopLeft, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TopLeft.Panel1.SuspendLayout()
+        Me.TopLeft.Panel2.SuspendLayout()
+        Me.TopLeft.SuspendLayout()
         CType(Me.logdata, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.logdata.Panel1.SuspendLayout()
         Me.logdata.Panel2.SuspendLayout()
@@ -50,25 +63,27 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(430, 3)
+        Me.Button1.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Button1.Location = New System.Drawing.Point(561, 0)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.Size = New System.Drawing.Size(75, 25)
         Me.Button1.TabIndex = 0
         Me.Button1.Text = "Select File"
         Me.Button1.UseVisualStyleBackColor = True
         '
-        'TextBox1
+        'SourceFile
         '
-        Me.TextBox1.Location = New System.Drawing.Point(3, 5)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(424, 20)
-        Me.TextBox1.TabIndex = 1
+        Me.SourceFile.Location = New System.Drawing.Point(6, 3)
+        Me.SourceFile.Name = "SourceFile"
+        Me.SourceFile.Size = New System.Drawing.Size(549, 20)
+        Me.SourceFile.TabIndex = 1
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(511, 3)
+        Me.Button2.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Button2.Location = New System.Drawing.Point(561, 0)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.Size = New System.Drawing.Size(75, 26)
         Me.Button2.TabIndex = 2
         Me.Button2.Text = "Load"
         Me.Button2.UseVisualStyleBackColor = True
@@ -76,7 +91,7 @@ Partial Class Form1
         'RETR
         '
         Me.RETR.AutoSize = True
-        Me.RETR.Location = New System.Drawing.Point(3, 31)
+        Me.RETR.Location = New System.Drawing.Point(12, 6)
         Me.RETR.Name = "RETR"
         Me.RETR.Size = New System.Drawing.Size(96, 17)
         Me.RETR.TabIndex = 3
@@ -92,12 +107,7 @@ Partial Class Form1
         '
         'SplitContainer1.Panel1
         '
-        Me.SplitContainer1.Panel1.Controls.Add(Me.userSearch)
-        Me.SplitContainer1.Panel1.Controls.Add(Me.lblUser)
-        Me.SplitContainer1.Panel1.Controls.Add(Me.TextBox1)
-        Me.SplitContainer1.Panel1.Controls.Add(Me.RETR)
-        Me.SplitContainer1.Panel1.Controls.Add(Me.Button2)
-        Me.SplitContainer1.Panel1.Controls.Add(Me.Button1)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.topMain)
         '
         'SplitContainer1.Panel2
         '
@@ -106,6 +116,72 @@ Partial Class Form1
         Me.SplitContainer1.Size = New System.Drawing.Size(784, 561)
         Me.SplitContainer1.SplitterDistance = 55
         Me.SplitContainer1.TabIndex = 5
+        '
+        'topMain
+        '
+        Me.topMain.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.topMain.Location = New System.Drawing.Point(0, 0)
+        Me.topMain.Name = "topMain"
+        '
+        'topMain.Panel1
+        '
+        Me.topMain.Panel1.Controls.Add(Me.TopLeft)
+        '
+        'topMain.Panel2
+        '
+        Me.topMain.Panel2.Controls.Add(Me.LinesCounted)
+        Me.topMain.Size = New System.Drawing.Size(784, 55)
+        Me.topMain.SplitterDistance = 636
+        Me.topMain.TabIndex = 6
+        '
+        'TopLeft
+        '
+        Me.TopLeft.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TopLeft.Location = New System.Drawing.Point(0, 0)
+        Me.TopLeft.Name = "TopLeft"
+        Me.TopLeft.Orientation = System.Windows.Forms.Orientation.Horizontal
+        '
+        'TopLeft.Panel1
+        '
+        Me.TopLeft.Panel1.Controls.Add(Me.Button1)
+        Me.TopLeft.Panel1.Controls.Add(Me.SourceFile)
+        '
+        'TopLeft.Panel2
+        '
+        Me.TopLeft.Panel2.Controls.Add(Me.Button2)
+        Me.TopLeft.Panel2.Controls.Add(Me.RETR)
+        Me.TopLeft.Panel2.Controls.Add(Me.lblUser)
+        Me.TopLeft.Panel2.Controls.Add(Me.userSearch)
+        Me.TopLeft.Size = New System.Drawing.Size(636, 55)
+        Me.TopLeft.SplitterDistance = 25
+        Me.TopLeft.TabIndex = 0
+        '
+        'lblUser
+        '
+        Me.lblUser.AutoSize = True
+        Me.lblUser.Location = New System.Drawing.Point(386, 10)
+        Me.lblUser.Name = "lblUser"
+        Me.lblUser.Size = New System.Drawing.Size(32, 13)
+        Me.lblUser.TabIndex = 4
+        Me.lblUser.Text = "User:"
+        '
+        'userSearch
+        '
+        Me.userSearch.Location = New System.Drawing.Point(424, 7)
+        Me.userSearch.Name = "userSearch"
+        Me.userSearch.Size = New System.Drawing.Size(131, 20)
+        Me.userSearch.TabIndex = 5
+        '
+        'LinesCounted
+        '
+        Me.LinesCounted.AutoSize = True
+        Me.LinesCounted.Dock = System.Windows.Forms.DockStyle.Left
+        Me.LinesCounted.Location = New System.Drawing.Point(0, 0)
+        Me.LinesCounted.Name = "LinesCounted"
+        Me.LinesCounted.Size = New System.Drawing.Size(13, 13)
+        Me.LinesCounted.TabIndex = 0
+        Me.LinesCounted.Text = "0"
+        Me.LinesCounted.TextAlign = System.Drawing.ContentAlignment.TopRight
         '
         'logdata
         '
@@ -176,21 +252,15 @@ Partial Class Form1
         Me.exportData.Text = "Export Selected"
         Me.exportData.UseVisualStyleBackColor = True
         '
-        'lblUser
+        'BackgroundWorker1
         '
-        Me.lblUser.AutoSize = True
-        Me.lblUser.Location = New System.Drawing.Point(258, 31)
-        Me.lblUser.Name = "lblUser"
-        Me.lblUser.Size = New System.Drawing.Size(32, 13)
-        Me.lblUser.TabIndex = 4
-        Me.lblUser.Text = "User:"
+        Me.BackgroundWorker1.WorkerReportsProgress = True
+        Me.BackgroundWorker1.WorkerSupportsCancellation = True
         '
-        'userSearch
+        'bgWorker
         '
-        Me.userSearch.Location = New System.Drawing.Point(296, 29)
-        Me.userSearch.Name = "userSearch"
-        Me.userSearch.Size = New System.Drawing.Size(131, 20)
-        Me.userSearch.TabIndex = 5
+        Me.bgWorker.WorkerReportsProgress = True
+        Me.bgWorker.WorkerSupportsCancellation = True
         '
         'Form1
         '
@@ -199,12 +269,22 @@ Partial Class Form1
         Me.ClientSize = New System.Drawing.Size(784, 561)
         Me.Controls.Add(Me.SplitContainer1)
         Me.Name = "Form1"
-        Me.Text = "Choose Log File"
+        Me.Text = "Log Reader"
         Me.SplitContainer1.Panel1.ResumeLayout(False)
-        Me.SplitContainer1.Panel1.PerformLayout()
         Me.SplitContainer1.Panel2.ResumeLayout(False)
         CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.SplitContainer1.ResumeLayout(False)
+        Me.topMain.Panel1.ResumeLayout(False)
+        Me.topMain.Panel2.ResumeLayout(False)
+        Me.topMain.Panel2.PerformLayout()
+        CType(Me.topMain, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.topMain.ResumeLayout(False)
+        Me.TopLeft.Panel1.ResumeLayout(False)
+        Me.TopLeft.Panel1.PerformLayout()
+        Me.TopLeft.Panel2.ResumeLayout(False)
+        Me.TopLeft.Panel2.PerformLayout()
+        CType(Me.TopLeft, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.TopLeft.ResumeLayout(False)
         Me.logdata.Panel1.ResumeLayout(False)
         Me.logdata.Panel2.ResumeLayout(False)
         CType(Me.logdata, System.ComponentModel.ISupportInitialize).EndInit()
@@ -214,7 +294,7 @@ Partial Class Form1
 
     End Sub
     Friend WithEvents Button1 As Button
-    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents SourceFile As TextBox
     Friend WithEvents Button2 As Button
     Friend WithEvents RETR As CheckBox
     Friend WithEvents SplitContainer1 As SplitContainer
@@ -228,4 +308,9 @@ Partial Class Form1
     Friend WithEvents Column5 As DataGridViewTextBoxColumn
     Friend WithEvents userSearch As TextBox
     Friend WithEvents lblUser As Label
+    Friend WithEvents topMain As SplitContainer
+    Friend WithEvents TopLeft As SplitContainer
+    Friend WithEvents LinesCounted As Label
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents bgWorker As System.ComponentModel.BackgroundWorker
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,5 +1,20 @@
 ﻿Imports System.Text.RegularExpressions
 Public Class Form1
+
+    Dim strRow As String()
+    Dim intCount As Int16
+    Dim dblAmount As Double             'Variable for total amount 
+    Dim blnReported As Boolean = True   'Flag to check progress report completed or not
+
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        'Allow background operation to be cancelled
+        bgWorker.WorkerSupportsCancellation = True
+
+        'Allow background operation to report progress
+        bgWorker.WorkerReportsProgress = True
+    End Sub
+
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Dim openFileDialog1 As New OpenFileDialog()
         openFileDialog1.InitialDirectory = CurDir.ToString
@@ -9,35 +24,39 @@ Public Class Form1
 
         If openFileDialog1.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
             ' Assign the cursor in the Stream to the Form's Cursor property.
-            TextBox1.Text = openFileDialog1.FileName.ToString
+            SourceFile.Text = openFileDialog1.FileName.ToString
         End If
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        If My.Computer.FileSystem.FileExists(TextBox1.Text) Then
-            logView.Rows.Clear()
-            Dim TextLine As String = ""
-            Dim findUser As String = ".*?"
-            If userSearch.Text <> "" Then
-                findUser = userSearch.Text
-            End If
-            'git
-            Using objReader As New System.IO.StreamReader(TextBox1.Text)
-                'uid..datetime..user..ip..message
-                Dim pattern As String = "(.*?)\s+(.*?)\s+\-\s+(" & findUser & ")\s+\((.*?)\)\>(.*)"
-                If RETR.Checked.ToString = "True" Then
-                    pattern = "(.*?)\s+(.*?)\s+\-\s+(" & findUser & ")\s+\((.*?)\)\>\s+(RETR .*)"
-                End If
-                Do While objReader.Peek() <> -1
-                    TextLine = objReader.ReadLine()
-                    Dim r As Regex = New Regex(pattern)
-                    If (r.IsMatch(TextLine)) Then
-                        Dim m As Match = r.Match(TextLine)
-                        logView.Rows.Add({m.Groups(1), m.Groups(2), m.Groups(3), m.Groups(4), m.Groups(5)})
-                    End If
-                Loop
-            End Using
+        If My.Computer.FileSystem.FileExists(SourceFile.Text) Then
 
+            logView.Rows.Clear()
+            intCount = 0
+            Call bgWorker.RunWorkerAsync()
+            If 1 = 2 Then ' Begin Never Run
+                Dim TextLine As String = ""
+                Dim findUser As String = ".*?"
+                If userSearch.Text <> "" Then
+                    findUser = userSearch.Text
+                End If
+                'git
+                Using objReader As New System.IO.StreamReader(SourceFile.Text)
+                    'uid..datetime..user..ip..message
+                    Dim pattern As String = "(.*?)\s+(.*?)\s+\-\s+(" & findUser & ")\s+\((.*?)\)\>(.*)"
+                    If RETR.Checked.ToString = "True" Then
+                        pattern = "(.*?)\s+(.*?)\s+\-\s+(" & findUser & ")\s+\((.*?)\)\>\s+(RETR .*)"
+                    End If
+                    Do While objReader.Peek() <> -1
+                        TextLine = objReader.ReadLine()
+                        Dim r As Regex = New Regex(pattern)
+                        If (r.IsMatch(TextLine)) Then
+                            Dim m As Match = r.Match(TextLine)
+                            logView.Rows.Add({m.Groups(1), m.Groups(2), m.Groups(3), m.Groups(4), m.Groups(5)})
+                        End If
+                    Loop
+                End Using
+            End If ' Never Run
         Else
             MsgBox("Error: File not found.")
         End If
@@ -71,5 +90,62 @@ Public Class Form1
         End If
 
 
+    End Sub
+
+    Private Sub bgWorker_DoWork(ByVal sender As Object, ByVal e As System.ComponentModel.DoWorkEventArgs) Handles bgWorker.DoWork
+        Dim TextLine As String = ""
+        Dim findUser As String = ".*?"
+        If userSearch.Text <> "" Then
+            findUser = userSearch.Text
+        End If
+        'git
+        Using objReader As New System.IO.StreamReader(SourceFile.Text)
+            'uid..datetime..user..ip..message
+            Dim pattern As String = "(.*?)\s+(.*?)\s+\-\s+(" & findUser & ")\s+\((.*?)\)\>(.*)"
+            If RETR.Checked.ToString = "True" Then
+                pattern = "(.*?)\s+(.*?)\s+\-\s+(" & findUser & ")\s+\((.*?)\)\>\s+(RETR .*)"
+            End If
+            Do While objReader.Peek() <> -1
+                If bgWorker.CancellationPending Then
+                    e.Cancel = True
+                    Exit Sub
+                Else
+                    While Not blnReported
+                        Application.DoEvents()
+                    End While
+                    TextLine = objReader.ReadLine()
+                    Dim r As Regex = New Regex(pattern)
+                    If (r.IsMatch(TextLine)) Then
+                        Dim m As Match = r.Match(TextLine)
+                        strRow = {m.Groups(1).ToString, m.Groups(2).ToString, m.Groups(3).ToString, m.Groups(4).ToString, m.Groups(5).ToString}
+                        blnReported = False
+                        intCount += 1
+                        bgWorker.ReportProgress(10, intCount)
+                    End If
+                End If
+                System.Threading.Thread.Sleep(5)
+            Loop
+        End Using
+
+    End Sub
+
+    Private Sub bgWorker_ProgressChanged(ByVal sender As Object, ByVal e As System.ComponentModel.ProgressChangedEventArgs) Handles bgWorker.ProgressChanged
+        'Copy values to data grid
+        logView.Rows.Add(strRow)
+        Me.LinesCounted.Text = intCount
+        blnReported = True
+
+    End Sub
+
+    Private Sub bgWorker_RunWorkerCompleted(ByVal sender As Object, ByVal e As System.ComponentModel.RunWorkerCompletedEventArgs) Handles bgWorker.RunWorkerCompleted
+
+        If e.Cancelled Or Not IsNothing(e.Error) Then
+            'Clear the data grid
+            logView.Rows.Clear()
+            Me.LinesCounted.Text = 0
+        Else
+            'to come
+
+        End If
     End Sub
 End Class
